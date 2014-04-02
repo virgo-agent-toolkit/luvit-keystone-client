f96c2a473f93979daca8aa3dc9c5b8f1aaed3d47
Ryan Phillips <ryan.phillips@rackspace.com>
2014-04-01 23:32:50 -0500

    initial pass at TFA support

--- lib/init.lua
+++ lib/init.lua
@@ -17,6 +17,7 @@ local JSON = require('json')
 local Object = require('core').Object
 local Error = require('core').Error
 local https = require('https')
+local http = require('http')
 local table = require('table')
 local fmt = require('string').format
 local url = require('url')
@@ -28,6 +29,12 @@ function Client:initialize(authUrl, options)
   self.apikey = options.apikey
   self.password = options.password
   self.extraArgs = options.extraArgs or {}
+  self.tfaCallback = options.tfaCallback
+  if authUrl:find('http:') then
+    self._proto = http
+  else
+    self._proto = https
+  end
   self._token = nil
   self._tokenExpires = nil
   self._tenantId = nil
@@ -35,89 +42,137 @@ function Client:initialize(authUrl, options)
 
 end
 
+function Client:setTFACallback(callback)
+  self.tfaCallback = callback
+end
+
 function Client:_updateToken(callback)
-  local body = {}
   local parsed = url.parse(self.authUrl)
-  local urlPath
-  local options
-  local headers
-  local client
-  local body
 
-  headers = {}
-  headers['Accept'] = 'application/json'
-  headers['Content-Type'] = 'application/json'
+  local iter
+  iter = function(tfaOptions)
+    local client
+    local body
+    local options
+    local headers = {}
+    headers['Accept'] = 'application/json'
+    headers['Content-Type'] = 'application/json'
 
-  urlPath = fmt('%s/tokens', parsed.pathname)
+    local urlPath = fmt('%s/tokens', parsed.pathname)
 
-  if self.password then
-    body = {
-      ['auth'] = {
-        ['passwordCredentials'] = {
-          ['username'] = self.username,
-          ['password'] = self.password
+    if tfaOptions then
+      headers['X-SessionId'] = tfaOptions.session_id
+      body = {
+        ['auth'] = {
+          ['RAX-AUTH:passcodeCredentials'] = {
+            ['passcode'] = tfaOptions.passcode
+          }
         }
       }
-    }
-  else
-    body = {
-      ['auth'] = {
-        ['RAX-KSKEY:apiKeyCredentials'] = {
-          ['username'] = self.username,
-          ['apiKey'] = self.apikey
+    elseif self.password then
+      body = {
+        ['auth'] = {
+          ['passwordCredentials'] = {
+            ['username'] = self.username,
+            ['password'] = self.password
+          }
         }
       }
+    else
+      body = {
+        ['auth'] = {
+          ['RAX-KSKEY:apiKeyCredentials'] = {
+            ['username'] = self.username,
+            ['apiKey'] = self.apikey
+          }
+        }
+      }
+    end
+
+    body = JSON.stringify(body)
+    headers['Content-Length'] = #body
+    options = {
+      host = parsed.hostname,
+      port = tonumber(parsed.port),
+      path = urlPath,
+      headers = headers,
+      method = 'POST'
     }
+
+    local function handleTFAResponse(res)
+      if res.headers['www-authenticate'] then
+        local auth = res.headers['www-authenticate']
+        local sidx = auth:find('\'')
+        local eidx = auth:find('\'', sidx + 1)
+        local tfa_options = {}
+        tfa_options.session_id = auth:sub(sidx + 1, eidx - 1)
+        tfa_options.passcode = nil
+        if self.tfaCallback then
+          self.tfaCallback(function(err, passcode)
+            if err then
+              callback(err)
+            else
+              tfa_options.passcode = passcode
+              iter(tfa_options)
+            end
+          end)
+        end
+      else
+        callback(Error:new('Not authenticated'))
+      end
+    end
+
+    local function handleTokenResponse(res)
+      local data = ''
+      res:on('data', function(chunk)
+        data = data .. chunk
+      end)
+      res:on('end', function()
+        print(data)
+        local json, payload, newToken, newExpires
+        local results  = {
+          xpcall(function()
+            return JSON.parse(data)
+          end, function(err)
+            return err
+          end)
+        }
+        -- protected call errored out
+        if not results[1] then
+          callback(results[1])
+          return
+        end
+        payload = results[2]
+
+        if payload.access then
+          newToken = payload.access.token.id
+          newExpires = payload.access.token.expires
+        else
+          callback(Error:new('Invalid response from auth server'))
+          return
+        end
+
+        self._token = newToken
+        self._tokenExpires = newExpires
+        self._serviceCatalog = payload.access.serviceCatalog
+
+        callback(nil, self._token)
+      end)
+    end
+
+    local function handleResponse(res)
+      if res.statusCode == 401 then
+        handleTFAResponse(res)
+      else
+        handleTokenResponse(res)
+      end
+    end
+
+    client = self._proto.request(options, handleResponse)
+    client:done(body)
   end
 
-  body = JSON.stringify(body)
-  headers['Content-Length'] = #body
-
-  options = {
-    host = parsed.hostname,
-    port = tonumber(parsed.port),
-    path = urlPath,
-    headers = headers,
-    method = 'POST'
-  }
-
-  client = https.request(options, function(res)
-    local data = ''
-    res:on('data', function(chunk)
-      data = data .. chunk
-    end)
-    res:on('end', function()
-      local json, payload, newToken, newExpires
-      local results  = {
-        xpcall(function()
-          return JSON.parse(data)
-        end, function(err)
-          return err
-        end)
-      }
-      -- protected call errored out
-      if not results[1] then
-        callback(results[1])
-        return
-      end
-      payload = results[2]
-
-      if payload.access then
-        newToken = payload.access.token.id
-        newExpires = payload.access.token.expires
-      else
-        callback(Error:new('Invalid response from auth server'))
-        return
-      end
-
-      self._token = newToken
-      self._tokenExpires = newExpires
-      self._serviceCatalog = payload.access.serviceCatalog
-
-      callback(nil, self._token)
-    end)
-  end)
-  client:done(body)
+  iter()
 end
 
 function Client:tenantIdAndToken(callback)
