7e117e7dde2dbabdd2cf1db168250792a828f8c8
Ryan Phillips <ryan.phillips@rackspace.com>
2015-04-18 14:14:22 -0500

    bump deps

--- package.lua
+++ package.lua
@@ -1,11 +1,11 @@
 return {
   name = "virgo-agent-toolkit/keystone-client",
-  version = "0.3.1",
+  version = "0.3.2",
   description = "Rackspace Monitoring Client",
   author = "Ryan Phillips <ryan.phillips@rackspace.com>",
   dependencies = {
-    "luvit/luvit@2.0.1",
-    "rphillips/async@0.0.2",
+    "luvit/luvit@2",
+    "virgo-agent-toolkit/async@1",
     "virgo-agent-toolkit/request@0.2.4"
   },
   files = {
